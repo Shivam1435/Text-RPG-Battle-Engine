8d7c6fa37d255265644c2b5dd54391ff0ae89bf8
Sushant979 <sushantranjan0603@gmail.com>
2026-07-22 20:27:45 +0530

    Add Boss class definition inheriting from Enemy

--- include/Boss.h
+++ include/Boss.h
@@ -1,1 +1,30 @@
+#ifndef BOSS_H
+#define BOSS_H
 
+#include "Enemy.h"
+
+class Boss : public Enemy
+{
+private:
+    int specialAttackDamage;
+
+public:
+    Boss(
+        std::string name,
+        int level,
+        int hp,
+        int mp,
+        int attack,
+        int defense,
+        int speed,
+        int expReward,
+        int goldReward,
+        int specialDamage
+    );
+
+    int getSpecialAttackDamage() const;
+
+    void specialAttack(Character& target);
+};
+
+#endif
